3a9e3cb5c3c7470b35736405257d4eee8b344abd
jayanna G <jayannag580@gmail.com>
2023-09-23 17:54:35 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MyNamespace
+{
+    class Program
+    {
+         public static void Main(string[] args)
+        {
+            System.Console.WriteLine("enter your name");
+            String a = Console.ReadLine();
+            System.Console.WriteLine(a+"          hgahgdjh");
+        }
+    }
+}
---
+++ p1.cs
@@ -0,0 +1,12 @@
+namespace Name
+{
+    class p1
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("enter your name");
+            String a= Console.ReadLine();
+            System.Console.WriteLine(a);
+        }
+    }
+}
